28ebf52982710c4b72e7236d272c4a2285632466
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2020-02-02 17:02:38 -0500

    :wrench: update price to use more accurate data type

--- models/Budget.js
+++ models/Budget.js
@@ -1,31 +1,44 @@
-const Money = require("./Money").schema;
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const budgetSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: "Please provide a title for this budget"
+const setDecimalNumber = val => {
+  return (val * 100).toFixed(2);
+};
+
+const getDecimalNumber = val => {
+  return (val / 100).toFixed(2);
+};
+
+const budgetSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: "Please provide a title for this budget"
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId
+    },
+    price: {
+      type: Number,
+      default: 0,
+      set: setDecimalNumber,
+      get: getDecimalNumber
+    },
+    month: Number,
+    day: Number,
+    year: Number,
+    userId: Number,
+    created: {
+      type: Date,
+      default: Date.now
+    }
   },
-  description: {
-    type: String,
-    trim: true
-  },
-  categoryId: {
-    type: mongoose.Schema.Types.ObjectId
-  },
-  price: {
-    type: String //TODO: add support for Money schema
-  },
-  month: Number,
-  day: Number,
-  year: Number,
-  userId: Number,
-  created: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { toJSON: { getters: true }, toObject: { getters: true } }
+);
 
 module.exports = mongoose.model("Budget", budgetSchema);
